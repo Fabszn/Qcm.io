7b8d03583c41af29425089f523202275f6a5b704
Fabrice Sznajderman <fabszn@protonmail.com>
2021-03-07 21:17:57 +0100

    gohead

--- build.sbt
+++ build.sbt
@@ -2,11 +2,11 @@ version := "1.0-SNAPSHOT"
 
 scalaVersion in ThisBuild := "2.12.13"
 
-lazy val zioVersion = "1.0.1"
+lazy val zioVersion = "1.0.4-2"
 lazy val `zio-interop` = "2.3.1.0"
 
 lazy val zio = "dev.zio" %% "zio" %  zioVersion
-lazy val Http4sVersion = "0.21.3"
+lazy val Http4sVersion = "0.21.19"
 lazy val http4sBlazeServer = "org.http4s"  %% "http4s-blaze-server" % Http4sVersion
 lazy val http4sBlazeClient = "org.http4s"  %% "http4s-blaze-client" % Http4sVersion
 lazy val http4sDsl = "org.http4s"      %% "http4s-dsl"          % Http4sVersion
--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,15 +1,15 @@
 package http.server
 
-import cats.implicits._
-import org.http4s.implicits._
-import zio._
-import zio.interop.catz._
-import zio.interop.catz.implicits._
-import http.server.zhx.servers.Hello1Service
+import cats.effect._
 import fs2.Stream.Compiler._
-
+import http.server.zhx.servers.Hello1Service
+import org.http4s.HttpApp
+import org.http4s.implicits._
+import org.http4s.server.Router
+import org.http4s.server.blaze.BlazeServerBuilder
+import zio.interop.catz._
+import zio.{ExitCode => ZExitCode, _}
 import org.http4s.server.blaze._
-
 
 object MyApp extends zio.App {
 
@@ -19,14 +19,14 @@ object MyApp extends zio.App {
     val server: ZIO[ZEnv, Throwable, Unit] = ZIO.runtime[ZEnv]
       .flatMap {
         implicit rts =>
-          BlazeServerBuilder[Task]
+          BlazeServerBuilder.apply[Task](rts.platform.executor.asEC,)
             .bindHttp(8080, "localhost")
             .withHttpApp(Hello1Service.service)
             .serve
-            .compile
+            .compile[Task,Task, ExitCode]
             .drain
       }
 
   def run(args: List[String]) =
-    server.fold(_ =>  ExitCode.failure , _ => ExitCode.success)
+    server.fold(_ =>  ZExitCode.failure , _ => ZExitCode.success)
 }
